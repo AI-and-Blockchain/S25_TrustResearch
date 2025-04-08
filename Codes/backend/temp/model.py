c0acc5ca8593c87f5509055ee0d4ef11aeb13e1a
Ben Manicke <bsmanicke@gmail.com>
2025-04-08 06:39:22 -0400

    Readding-test-files
    after my merge issue. I am readding the test files which were saved to my local machine.

---
+++ Codes/backend/temp/model.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+def create_model(input_shape, num_classes):
+    model = Sequential()
+    model.add(Dense(64, activation='relu', input_shape=input_shape))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    # Compile the model
+    model.compile(optimizer=Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    return model
