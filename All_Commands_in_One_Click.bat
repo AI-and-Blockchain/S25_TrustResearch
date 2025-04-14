d9df38b08f5cb31c4ced35d41f6f9d4b9765a2d8
Md Motaleb Hossen Manik <manikm@rpi.edu>
2025-04-14 15:28:28 -0400

    commands in one file updated

--- All_Commands_in_One_Click.bat
+++ All_Commands_in_One_Click.bat
@@ -1,21 +1,32 @@
 @echo off
+
 echo Starting IPFS Daemon...
 start powershell -NoExit -Command "ipfs daemon"
+timeout /t 1 > nul
 
 echo Starting Ganache CLI...
 start powershell -NoExit -Command "ganache-cli --port 7545 --deterministic"
+timeout /t 4 > nul  REM Wait 4 seconds before Truffle
 
 echo Running Truffle Migrations...
 start powershell -NoExit -Command "cd Codes/blockchain; truffle migrate --reset"
+timeout /t 1 > nul
 
 echo Starting Backend Server...
 start powershell -NoExit -Command "cd Codes/backend; python server.py"
+timeout /t 1 > nul
 
 echo Starting Journal Receiver...
 start powershell -NoExit -Command "cd Codes/journal_receiver; python journal_receiver.py"
+timeout /t 1 > nul
 
-echo Starting Frontend...
-start powershell -NoExit -Command "cd Codes/frontend; npm start"
+echo Starting Frontend - Author...
+start powershell -NoExit -Command "cd Codes/frontend/author; npm start"
+timeout /t 1 > nul
+
+echo Starting Frontend - Reviewer...
+start powershell -NoExit -Command "cd Codes/frontend/reviewer; npm start"
+timeout /t 1 > nul
 
 echo All processes started.
 pause
